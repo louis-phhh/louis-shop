902232d63dc00aa5083f5f95668dbb18b0957db4
Louis Phan <louis.phhh@gmail.com>
2022-07-13 07:39:36 +0700

    update

---
+++ pages/products/[slug].tsx
@@ -0,0 +1,16 @@
+import useTrans from '@hooks/useTrans'
+import Layout from '@components/Layout'
+
+const ProductDetailt = () => {
+  const trans = useTrans()
+
+  return (
+    <div>
+      <p>Hậu Đẹp Trai</p>
+    </div>
+  )
+}
+
+ProductDetailt.Layout = Layout
+
+export default ProductDetailt
